5eb2e73fb6f59e9cb03d8d6f29ffa6b10b94487d
Dago <57285804+ColdMacaroni@users.noreply.github.com>
2021-03-03 11:23:48 +1300

    Add files via upload

--- suits.py
+++ suits.py
@@ -1,15 +1,16 @@
 ##
 # suits.py
-# suits abstract
-# Dago wrotten Dago abstracted
+# Asks the user to pick a suit, compares it to another card
+# returns if they guessed the correct suit.
+# Dago
 
 
 def suits(partial_history):
-    """Requires the last items of suit history. >=1"""
 
     global SUITS_DICT
 
     # Take the first character
+    # s if spades, c if clubs, h if hearts, d if diamonds
     choice = input("Next suit? ").strip().lower()[0]
 
     print('-'*(len(partial_history)+2))
@@ -26,6 +27,5 @@ def suits(partial_history):
         multiplier = 0
 
     return multiplier
-
 # Dict will be used to convert user input into symbol
 SUITS_DICT = {'s': '♤', 'h': '♡', 'c': '♧', 'd': '♢'}
