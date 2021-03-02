940d66767c280e8d17fb58c6567bfa88ed0c25b9
Dago <57285804+ColdMacaroni@users.noreply.github.com>
2021-03-03 10:18:19 +1300

    Fix ups

--- suits.py
+++ suits.py
@@ -1,11 +1,13 @@
 ##
 # suits.py
-# GAmble gaMBle gaMBLe
+# suits abstract
 # Dago
 
-from random import choice, shuffle
-
 def suits(partial_history)
+    """Requires the last items of suit history. >=1"""
+    
+    global SUITS_DICT
+    
     # Take the first character
     choice = input("Next suit? ").strip().lower()[0]
 
@@ -23,5 +25,6 @@ def suits(partial_history)
         multiplier = 0
 
     return multiplier
+
 # Dict will be used to convert user input into symbol
 SUITS_DICT = {'s': '♤', 'h': '♡', 'c': '♧', 'd': '♢'}
